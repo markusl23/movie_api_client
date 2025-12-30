c226bc91545d172f854b780de18ef3682dd3c6d4
markusl23 <markus4github@riseup.net>
2025-12-30 13:36:01 +0100

    removed hard-coded movie data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,32 +3,7 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-  	{
-  	  id: 1,
-  	  title: "The Shawshank Redemption",
-  	  director: "Frank Darabont",
-  	  genre: "Drama",
-  	  description: "Drama movies focus on intense character development and emotional storytelling, exploring real-life struggles, relationships, and personal growth through compelling, often thought-provoking narratives.",
-  	  poster: "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg"
-  	},
-  	{
-  	  id: 2,
-  	  title: "The Martian",
-  	  director: "Ridley Scott",
-  	  genre: "Science Fiction",
-  	  description: "The Martian tells the story of astronaut Mark Watney, who is stranded on Mars after a failed mission and must use his ingenuity to survive. Directed by Ridley Scott, the film explores themes of isolation, resilience, and human ingenuity in the face of extreme adversity. Watney must use his knowledge of science to grow food, generate oxygen, and communicate with Earth to survive.",
-  	  poster: "https://upload.wikimedia.org/wikipedia/en/c/cd/The_Martian_film_poster.jpg",
-  	},
-  	{
-  	  id: 3,
-  	  title: "Point Break",
-  	  director: "Kathryn Bigelow",
-  	  genre: "Action",
-  	  description: "Point Break is an action-packed thriller about an undercover FBI agent, Johnny Utah, who infiltrates a group of surfers suspected of robbing banks. Directed by Kathryn Bigelow, the film blends extreme sports and crime drama, exploring themes of freedom, risk, and loyalty. As Utah forms a bond with the group’s charismatic leader, Bodhi, his own beliefs about life are tested.",
-  	  poster: "https://upload.wikimedia.org/wikipedia/en/7/7e/Pointbreaktheatrical.jpg",
-  	}
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
