094d2039b67e0a2e713bd73b8eb1e1eabf5b58c9
markusl23 <markus4github@riseup.net>
2025-12-30 13:50:01 +0100

    updated main-view.jsx code to fetch real movie api data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -6,6 +6,25 @@ export const MainView = () => {
   const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://still-depths-22545-dbe8396f909e.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.movies.map((movie) => {
+          return {
+            id: movie._id,
+            title: movie.Title,
+            director: movie.Director,
+            genre: movie.Genre,
+            description: movie.Description,
+            poster: movie.ImagePath
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
   	return (
