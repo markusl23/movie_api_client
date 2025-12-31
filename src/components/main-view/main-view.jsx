920073e563d6811582dde978295ca595a1764155
markusl23 <markus4github@riseup.net>
2025-12-31 19:06:24 +0100

    updated main-view.jsx to use local storage auth data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,10 +3,12 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
 
   if (!user) {
     return (
@@ -78,7 +80,7 @@ export const MainView = () => {
         onClick={() => { 
           setUser(null);
           setToken(null);
-
+          localStorage.clear();
         }}
       >
         Logout
