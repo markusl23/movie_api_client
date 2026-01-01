163ffad1e233919c1e74cdb04645f56e7a0949d7
markusl23 <markus4github@riseup.net>
2026-01-01 17:17:46 +0100

    added bootstrap card display style to movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,15 @@
 import PropTypes from "prop-types";
+import { Button, Card } from 'react-bootstrap';
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-      	onMovieClick(movie);
-      }}
-    >
-      {movie.title}
-    </div>
+    <Card onClick={() => onMovieClick(movie)}>
+      <Card.Img variant="top" src={movie.poster} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.genre}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
