cd8405996804ab34491ecf5667d96fc986ea0c9e
markusl23 <markus4github@riseup.net>
2026-01-02 13:16:15 +0100

    imported & added React Container to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client';
+import Container from 'react-bootstrap/Container';
 import { MainView } from './components/main-view/main-view';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 const MovieApiApplication = () => {
-  return <MainView />
+  return (
+    <Container>
+      <MainView />
+    </Container>
 };
 
 const container = document.querySelector('#root');
