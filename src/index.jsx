e730b9322b3bde7d0dad037405db418cff7046e2
markusl23 <markus4github@riseup.net>
2026-01-02 16:57:13 +0100

    removed pre-compiled React Bootstrap CSS file import

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import Container from 'react-bootstrap/Container';
 import { MainView } from './components/main-view/main-view';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 const MovieApiApplication = () => {
