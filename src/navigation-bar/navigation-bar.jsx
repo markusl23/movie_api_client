c46a81efb05b7338500a1cc80ad51d77f272eb5a
markusl23 <markus4github@riseup.net>
2026-01-05 19:06:08 +0100

    added intial code for navigation-bar.jsx

---
+++ src/navigation-bar/navigation-bar.jsx
@@ -0,0 +1,39 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+export const NavigationBar = () => {
+  return (
+    <>
+      {[false].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">Movie API Client</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}              
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Actions
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Navbar.Text>
+                      Signed in as: <a href="#login">Mark Otto</a>
+                  </Navbar.Text>
+                  <Nav.Link href="#action1">User Profile</Nav.Link>
+                  <Nav.Link href="#action2">Log out</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+};
