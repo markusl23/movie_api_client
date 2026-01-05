aae56db55f2760fa5165274562a074825b0aa549
markusl23 <markus4github@riseup.net>
2026-01-05 17:59:54 +0100

    fixed typo

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const MovieView = ({ movies }) => {
   const { movieId } = useParams();
 
-  const movie = movie.find((m) => m.id === movieId);
+  const movie = movies.find((m) => m.id === movieId);
 
   return (
     <div>
